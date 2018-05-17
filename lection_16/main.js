fddfc0f492dc8cdf7777b3ce7ba06859c978ab47
lenka-zelenka <elenazologina@gmail.com>
2018-05-17 21:07:02 +0300

    merge HW_15 && HW_!6

--- lection_16/main.js
+++ lection_16/main.js
@@ -1,7 +1,38 @@
 window.onload = function() {
     var contextmenu = document.querySelector('#context-menu');
+    var block = document.querySelector('.block');
     var mousepositionX,
         mousepositionX;
+
+
+
+    acntionCtrl = {
+        JumpHandler: JumpHandler,
+        Remove: Remove,
+        ChangeColor: ChangeColor
+    }
+
+
+
+ function JumpHandler() {
+    Jump(block);
+ }
+    
+    function Jump(block){
+        setTimeout(function () {
+            block.style.top = block.offsetTop - h + 'px';
+        },200)
+        setTimeout(function () {
+            block.style.top = block.offsetTop + h + 'px';
+        },300)
+    }
+    function Remove(){
+        console.log('Paste')
+    }
+    function ChangeColor(){
+        console.log('Delete')
+    }
+
 
     document.addEventListener('contextmenu', function(event){
         event.preventDefault();
@@ -19,16 +50,16 @@ window.onload = function() {
     var cm = {
         actions: [
             {
-                title: 'Copy',
-                handler:  'Copy',
+                title: 'Jump',
+                handler:  'JumpHandler',
             },
             {
-                title: 'Paste',
-                handler:  'Paste',
+                title: 'Remove',
+                handler:  'Remove',
             },
             {
-                title: 'Delete',
-                handler:  'Delete',
+                title: 'Change color',
+                handler:  'ChangeColor',
             },
             
         ]
@@ -40,17 +71,64 @@ window.onload = function() {
         li.classList.add('context-menu-item');
         li.innerHTML = cm.actions[i].title;
         
-        li.addEventListener('mousedown', window[cm.actions[i].handler]);
+        li.addEventListener('mousedown', acntionCtrl[cm.actions[i].handler]);
         contextmenu.appendChild(li);
     }
-}
-function Copy(){
-    console.log('Copy')
-}
-function Paste(){
-    console.log('Paste')
-}
-function Delete(){
-    console.log('Delete')
+
+
+
+var h = 100,
+    step = 20,
+    direction = 1,
+    gap = 5;
+
+function sitDown() { 
+    console.dir(block)
+    if (block.clientHeight === 100) {
+        block.style.height = 50 + 'px'
+        block.style.top = block.offsetTop + 50 + 'px'
+    }
 }
 
+function getUp() {
+    if (block.clientHeight === 50) {
+        block.style.height = 100 + 'px'
+        block.style.top = block.offsetTop - 50 + 'px'
+    }
+}
+document.addEventListener('keyup', function(event){
+    if (!event.ctrlKey) {
+        getUp();
+    }
+})
+document.addEventListener('keydown', function(event){
+    console.log(event)
+    if (event.ctrlKey) {
+        sitDown();
+    }
+    switch(event.keyCode) {
+        case 32:
+            setTimeout(function () {
+                block.style.top = block.offsetTop - h + 'px';
+            },200)
+            setTimeout(function () {
+                block.style.top = block.offsetTop + h + 'px';
+            },300)
+            break;
+        case 37 : 
+            block.style.left = block.offsetLeft - step + 'px';
+            break
+        case 38: 
+            block.style.top = block.offsetTop - step + 'px'; 
+            break
+        case 39: 
+            block.style.left = block.offsetLeft + step + 'px';
+            break
+        case 40: 
+            block.style.top = block.offsetTop + step + 'px';
+    }
+})
+
+
+
+}
