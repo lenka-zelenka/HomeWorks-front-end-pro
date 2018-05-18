2e9f8cdefa7ab0c5c03ede7e9a3d6dec2b0e9b24
elenazologina <elenazologina@gmail.com>
2018-05-18 19:34:11 +0300

    HW_16 done

--- lection_16/main.js
+++ lection_16/main.js
@@ -1,10 +1,8 @@
-window.onload = function() {
+window.onload = function () {
     var contextmenu = document.querySelector('#context-menu');
     var block = document.querySelector('.block');
-    var mousepositionX,
-        mousepositionX;
-
-
+    var positionY = block.offsetTop,
+        positionX = block.offsetLeft;
 
     acntionCtrl = {
         JumpHandler: JumpHandler,
@@ -14,120 +12,163 @@ window.onload = function() {
 
 
 
- function JumpHandler() {
-    Jump(block);
- }
-    
-    function Jump(block){
+    function JumpHandler() {
+        jump(block);
+    }
+
+    function jump(block) {
         setTimeout(function () {
             block.style.top = block.offsetTop - h + 'px';
-        },200)
+        }, 200)
         setTimeout(function () {
             block.style.top = block.offsetTop + h + 'px';
-        },300)
+        }, 300)
+        isMoveing();
     }
-    function Remove(){
-        console.log('Paste')
+
+    function Remove() {
+        block.remove();
     }
-    function ChangeColor(){
-        console.log('Delete')
+
+    function ChangeColor() {
+        var color = prompt("Введите цвет");
+        block.style.backgroundColor = color;
     }
 
 
-    document.addEventListener('contextmenu', function(event){
-        event.preventDefault();
-             
-        contextmenu.style.left = event.pageX + 'px';
-        contextmenu.style.top = event.pageY + 'px';
-        contextmenu.classList.add('show');
-    })
-    document.addEventListener('mousedown', function(event){     
-        contextmenu.classList.remove('show');
-    }) 
+    
 
 
     // DATA
     var cm = {
-        actions: [
-            {
+        actions: [{
                 title: 'Jump',
-                handler:  'JumpHandler',
+                handler: 'JumpHandler',
             },
             {
                 title: 'Remove',
-                handler:  'Remove',
+                handler: 'Remove',
             },
             {
                 title: 'Change color',
-                handler:  'ChangeColor',
+                handler: 'ChangeColor',
             },
-            
+
         ]
     }
 
 
-    for(i=0; i < cm.actions.length; i++) {
+    for (i = 0; i < cm.actions.length; i++) {
         var li = document.createElement('li');
         li.classList.add('context-menu-item');
         li.innerHTML = cm.actions[i].title;
-        
         li.addEventListener('mousedown', acntionCtrl[cm.actions[i].handler]);
         contextmenu.appendChild(li);
     }
+    block.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+        // console.log(contextmenu.getBoundingClientRect())
+        contextmenu.classList.add('show');
+        if(event.pageX >= window.innerWidth - contextmenu.clientWidth){
+            contextmenu.style.left = event.pageX - contextmenu.clientWidth + 'px';
+        } else {
+            contextmenu.style.left = event.pageX + 'px';
+        }
+        if(event.pageY >= window.innerHeight - contextmenu.clientHeight){
+            contextmenu.style.top = event.pageY - contextmenu.clientHeight + 'px';
+        } else {
+            contextmenu.style.top = event.pageY + 'px';
+        }
+
+        
+        
+    })
+    document.addEventListener('mousedown', function (event) {
+        contextmenu.classList.remove('show');
+    })
 
 
+    var isMove =false;
 
-var h = 100,
-    step = 20,
-    direction = 1,
-    gap = 5;
+    var h = 100,
+        step = 20,
+        gap = 5;
 
-function sitDown() { 
-    console.dir(block)
-    if (block.clientHeight === 100) {
-        block.style.height = 50 + 'px'
-        block.style.top = block.offsetTop + 50 + 'px'
+    function sitDown() {
+        if (block.clientHeight === 100) {
+            block.style.height = 50 + 'px'
+            moveTo('top', 'offsetTop', -h/2 )
+        }
     }
-}
 
-function getUp() {
-    if (block.clientHeight === 50) {
-        block.style.height = 100 + 'px'
-        block.style.top = block.offsetTop - 50 + 'px'
+    function getUp() {
+        if (block.clientHeight === 50) {
+            block.style.height = 100 + 'px'
+            moveTo('top', 'offsetTop', h/2 )
+        }
     }
-}
-document.addEventListener('keyup', function(event){
-    if (!event.ctrlKey) {
-        getUp();
+    function moveTo(direction, position, step){
+        block.style[direction] = block[position] - step + 'px';
     }
-})
-document.addEventListener('keydown', function(event){
-    console.log(event)
-    if (event.ctrlKey) {
-        sitDown();
+    function isMoveing(){
+        isMove = true;
+        setTimeout(function() {
+            isMove = false;
+        }, 100)
     }
-    switch(event.keyCode) {
-        case 32:
-            setTimeout(function () {
-                block.style.top = block.offsetTop - h + 'px';
-            },200)
-            setTimeout(function () {
-                block.style.top = block.offsetTop + h + 'px';
-            },300)
-            break;
-        case 37 : 
-            block.style.left = block.offsetLeft - step + 'px';
-            break
-        case 38: 
-            block.style.top = block.offsetTop - step + 'px'; 
-            break
-        case 39: 
-            block.style.left = block.offsetLeft + step + 'px';
-            break
-        case 40: 
-            block.style.top = block.offsetTop + step + 'px';
-    }
-})
+
+    document.addEventListener('keyup', function (event) {
+        if (!event.ctrlKey) {
+            getUp();
+        }
+    })
+    document.addEventListener('keydown', function (event) {
+        // console.log(event)
+        if (event.ctrlKey) {
+            sitDown();
+        }
+
+        if (isMove) return;
+        switch (event.keyCode) {
+            case 32:
+                // setTimeout(function () {
+                //     block.style.top = block.offsetTop - h + 'px';
+                // }, 200)
+                // setTimeout(function () {
+                //     block.style.top = block.offsetTop + h + 'px';
+                // }, 300)
+                jump(block);
+                
+                break;
+            case 37:
+                // ArrowLeft
+                if(block.offsetLeft >= step) {
+                    moveTo('left', 'offsetLeft', step);
+                    isMoveing();
+                }
+                break
+            case 38:
+                // ArrowUp
+                if(block.offsetTop >= step){
+                    moveTo('top', 'offsetTop', step)
+                    isMoveing();
+                }
+                break
+            case 39:
+                // ArrowRight
+                if(block.offsetLeft + block.clientWidth + step <= window.innerWidth ){
+                    moveTo('left', 'offsetLeft', -step)
+                    isMoveing();
+                }
+                break
+            case 40:
+                // ArrowDown
+                if(block.offsetTop + block.clientHeight + step <= window.innerHeight){
+                    moveTo('top', 'offsetTop', -step );
+                    isMoveing();
+                }
+        }
+    })
 
 
 
