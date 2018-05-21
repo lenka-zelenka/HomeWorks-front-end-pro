bb36fa53c8c3f4f1f14f606b9d3b063ef1b0edb2
lenka-zelenka <elenazologina@gmail.com>
2018-05-21 20:55:43 +0300

    array methods

---
+++ lection_17/main.js
@@ -0,0 +1,29 @@
+//  перебирающие методы
+// 
+
+window.onload = function(){
+    // var buttons = document.querySelectorAll('[class^="btn"]');
+    // console.log(buttons)
+    // buttons.forEach(function(item){
+    //     item.addEventListener('click', function(){
+    //         console.log(item);
+    //     })
+    // })
+
+
+    data = [1, 2, 3, 'slkvjlskfjsf', {x : 3}];
+
+    myMethods = {
+         getValuesByType: function(type) {
+            if (typeof item ==  type) { 
+                return item
+            }       
+        },
+    };
+  
+    var res = myMethods.getValuesByType.call(data, 'string');
+    console.log(res);
+
+
+    
+}
