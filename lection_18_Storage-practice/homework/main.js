5d464c3f48669108f1bb661ba4a5687c08fb7959
lenka-zelenka <elenazologina@gmail.com>
2018-06-06 23:55:30 +0300

    HW_18 bad cart((((

--- lection_18_Storage-practice/homework/main.js
+++ lection_18_Storage-practice/homework/main.js
@@ -1,52 +1,62 @@
-// 1
-// напиши функцию pluck, которая берет массив объектов и возвращает массив значений определенного поля:
+function doAjax(method, path){
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open(method, path, true);
+    xhr.send();
+    return new Promise(function(resolve, reject){
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState != 4) {
+                return;
+            }
+            if (xhr.status != 200) {
+                reject(xhr);
+            } 
+            resolve(JSON.parse(xhr.responseText));
+        }
+    })
+}
+doAjax('GET', './goods.json')
+    .then(function(response){
+        goods = response;
+    })
 
-// var characters = [
-//     { 'name': 'barney', 'age': 36 },
-//     { 'name': 'fred', 'age': 40 }
-// ];
+var addBtn = document.querySelectorAll('.add-to-cart')
 
-// console.log(pluck(characters, 'name')); // ['barney', 'fred']
-
-
-var characters = [{
-        'name': 'barney',
-        'age': 36
-    },
-    {
-        'name': 'fred',
-        'age': 40
+addBtn.forEach(function(item){
+    item.addEventListener('click', addToCart);
+});
+// localCart =  JSON.parse(localStorage.getItem("goods"));
+function addToCart (event){
+    itemId = this.parentNode.parentNode.id;
+    // selectedGood ={};
+    selectedGood = goods.goods[itemId];
+    selectedGood.quantity = 1;
+    console.log(this.parentNode.parentNode.id, selectedGood); 
+    setLocalGoods(selectedGood);
+    InitGoodsCart(selectedGood)
+}
+function setLocalGoods(items) {
+    localStorage.setItem('goods', JSON.stringify(items));
+}
+function getLocalGoods(){
+    return localStorage.getItem('goods')
+}
+function InitGoodsCart(cartItems){
+    tempitems = cartItems;
+    console.log(cartItems.goods.length)
+    var basket = document.querySelector('#basket .collection');
+    for (var i = 0; i < cartItems.goods.length; i++ ){
+        if(cartItems.goods[i].quantity){
+            console.log(cartItems.goods[i].title)
+            
+            var li = document.createElement('li')
+            li.classList.add('collection-item');
+            li.innerHTML =  '<span class="item-title">'+ cartItems.goods[i].title +
+                ' </span>' + '<span class="item-price">'+ cartItems.goods[i].price + 
+                ' </span>' + '<span class="new badge">' + cartItems.goods[i].quantity + '</span>';
+            basket.appendChild(li);
+            
+        }
+        // console.log(cartItems.goods[i].title)
     }
-];
-function pluck(arr, value) {
-    return arr.map(function (item) {
-        return item[value];
-    });
-}
-console.log(pluck(characters, 'name')); // ['barney', 'fred']
-
-// 2
-// напиши функцию filter, которая принимает функцию-предикат и массив. 
-// Возвращает она массив значений, для которых предикат вернет true.
-
-var input = [1, 2, 3, 4, 5, 6];
-function isEven(x) { return x % 2 == 0; } // проверяет на четность
-console.log(filter(input, isEven)); // [2, 4, 6]
-// Функция не должна изменять исходный массив
-
-function filter(arr, callback){
-    return arr.filter(callback)
-}
-
-// Array.prototype.foEach = function(callback){
-//     for(var i = 0; i < this.length; i++){
-//         callback(this[i], i, this);
-//     }
-// }
-
-
-// 3
-// Напиши функцию count(), считающую число свойств в объекте. Функция должна хранится в прототипе всех обьеков
-Object.prototype.count = function(){
-    return this.length
 }
