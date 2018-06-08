e34f4a6553b262331d184af42ce87d6272cd7b22
lenka-zelenka <elenazologina@gmail.com>
2018-06-09 00:51:54 +0300

    , render cards, add totalsum

--- lection_18_Storage-practice/homework/main.js
+++ lection_18_Storage-practice/homework/main.js
@@ -4,8 +4,9 @@ var model = {
 var addBtn = document.querySelectorAll('.add-to-cart');
 var clearBtn = document.querySelector('#clear-basket');
 var basket = document.querySelector('#basket .collection');
+var basketContainer =  document.getElementById('basket')
 var removeItemBtns = [];
-
+var totalSum = 0;
 function doAjax(method, path){
     var xhr = new XMLHttpRequest();
     
@@ -26,16 +27,52 @@ function doAjax(method, path){
 doAjax('GET', './goods.json')
     .then(function(response){
         model.goods = response.goods;
+        renderGoods()
     })
 
+function renderGoods(){
+    var goodsConteiner = document.getElementById('goods-container')
+    for (key in model.goods) {
+        var col = document.createElement('div');
+        col.className = "col s12 m4 l3";
 
-addBtn.forEach(function(item){
-    item.addEventListener('click', addToCart);
-});
+        var card = document.createElement('div');
+        card.className = "card"
+        card.setAttribute('id', model.goods[key].id)
+
+        var cardImage = document.createElement('div');
+        cardImage.className = "card-image"
+        cardImage.innerHTML = ' <img src="' + model.goods[key].imageSrc + ' ">'
+
+        var cardContent = document.createElement('div');
+        cardContent.className = "card-content";
+        cardContent.innerHTML = '<span class="card-title"> ' + model.goods[key].title + '</span> '+ 
+        '<span class="tial"> ' + model.goods[key].price + '</span> ';
+
+        var cardAction = document.createElement('div');
+        cardAction.className = 'card-action';
+
+
+        var addBtn = document.createElement('a');
+        addBtn.className = 'add-to-cart';
+        addBtn.innerText = 'В корзину!';
+        addBtn.addEventListener('click', addToCart);
+
+        cardAction.appendChild(addBtn);
+        card.appendChild(cardImage);
+        card.appendChild(cardContent);
+        card.appendChild(cardAction);
+        col.appendChild(card);        
+        goodsConteiner.appendChild(col);
+        
+    }
+}
+
 clearBtn.addEventListener('click', function(event){
     clearBasket(basket);
     //очищаем все хранилище
     delete localStorage.goods;
+    basketContainer.style.display  = 'none';
 });
 removeItemBtns.forEach(function(item){
     item.addEventListener('click', clearBasketItem)
@@ -68,42 +105,48 @@ function getLocalGoods(){
     return JSON.parse(localStorage.goods);
 }
 function InitGoodsCart(){
-    tempitems = getLocalGoods();
-    
+    showBasket();
+    var tempitems = getLocalGoods();
+    var sum = 0;
     if (basket) {
         clearBasket(basket);
     }
-    for (key in tempitems){        
+    for (key in tempitems){   
+        sum += (tempitems[key].price * tempitems[key].quantity) 
         var li = document.createElement('li');
         removeItemBtn = document.createElement('a');
-        removeItemBtn.classList.add('remove-item')
+        removeItemBtn.classList.add('remove-item', 'red-text')
         removeItemBtn.innerHTML = '<i class="material-icons">close</i>';
         removeItemBtn.addEventListener('click', function(event){
             clearBasketItem(tempitems[key])
         });
-        // removeItemBtn = '<a href="#" class="remove-item"><i class="material-icons">close</i></a>'
         li.classList.add('collection-item');
         li.innerHTML =  
             '<span class="item-title">'+ tempitems[key].title + ' </span>' + 
-            '<span class="item-price">'+ tempitems[key].price + ' </span>' + 
+            '<span class="item-price">'+ tempitems[key].price * tempitems[key].quantity + ' </span>' + 
             '<span class="new badge">' + tempitems[key].quantity + '</span>';
         li.appendChild(removeItemBtn)
         basket.appendChild(li);
-        // removeItemBtns.push(removeItemBtn)
     }
+    totalSum = sum;
+    totalContainer = document.getElementById('total-summ');
+    totalContainer.innerText = totalSum;
+}
+
+function showBasket(){
+    basketContainer.style.display  = 'block';
 }
 
 function clearBasket(basket) {
-    basket.querySelectorAll('li').forEach(item => item.remove());
-    
+    basket.querySelectorAll('li').forEach(item => item.remove());    
 }
 function clearBasketItem(item) {
-    console.log(item.id)
-    // item.remove();
-    // delete localStorage.goods[item];
-    // // InitGoodsCart();
-    var itemId = item.id;
-    var selectedGood = model.goods[itemId];
-    setLocalGoods(selectedGood);
-    InitGoodsCart(selectedGood)
+    var tempitems = getLocalGoods();  
+    totalSum = totalSum -  tempitems[item.id].price * tempitems[item.id].quantity;     
+    delete tempitems[item.id]; 
+    localStorage.setItem('goods', JSON.stringify(tempitems));
+    InitGoodsCart(tempitems);
+    if (Object.keys(tempitems).length == 0) {
+        basketContainer.style.display  = 'none';
+    }
 }
