62bbb6df71ea3d78885f2262cfc2f8f7989b3358
lenka-zelenka <elenazologina@gmail.com>
2018-06-26 01:20:38 +0300

    append method

--- lection_22_ES6_class/homework/main.js
+++ lection_22_ES6_class/homework/main.js
@@ -1,17 +1,26 @@
 class Element {
     constructor(selector) {
         this.selector = document.createElement(selector);
-        
-        document.body.appendChild(this.selector);
-        
+
     }
+    //запишет в текст элемента '!'
     html(text) {
-        this.selector.innerHtml = text;
-        // this.writeElem();
+        this.selector.innerText += text;
     }
-    // writeElem () {
-    //     debugger
-    // }
+    // запишет в начало элемента '!'
+    append(text) {
+        document.body.appendChild(this.selector)
+    }
+    prepend(text) {
+        // this.selector.insertBefore(this.selector, text)
+    }
 }
 var elem = new Element('div');
-elem.html('!')
+elem.html('!')//запишет в текст элемента '!'
+elem.append('!'); //запишет в начало элемента '!'
+elem.prepend('!'); //запишет в конец элемента '!'
+// elem.attr('class', 'www'); //запишет в атрибут class значение www
+// 
+// Должны работать цепочки методов:
+// elem.html('hello').append('!').prepend('!');
+// elem.attr('class', 'www').attr('title', 'hello');
