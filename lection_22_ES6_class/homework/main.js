d4ba71bdbd2d4277b62e9cc4851774792dbedb22
lenka-zelenka <elenazologina@gmail.com>
2018-06-27 23:45:59 +0300

    add user and copm  sheeps

--- lection_22_ES6_class/homework/main.js
+++ lection_22_ES6_class/homework/main.js
@@ -1,26 +1,72 @@
-class Element {
-    constructor(selector) {
-        this.selector = document.createElement(selector);
+// class Element {
+//     constructor(selector) {
+//         this.selector = document.createElement(selector);
+
+//     }
+//     //запишет в текст элемента '!'
+//     html(text) {
+//         this.selector.innerText += text;
+//     }
+//     // запишет в начало элемента '!'
+//     append(text) {
+//         document.body.appendChild(this.selector)
+//     }
+//     prepend(text) {
+//         // this.selector.insertBefore(this.selector, text)
+//     }
+// }
+// var elem = new Element('div');
+// elem.html('!')//запишет в текст элемента '!'
+// elem.append('!'); //запишет в начало элемента '!'
+// elem.prepend('!'); //запишет в конец элемента '!'
+// // elem.attr('class', 'www'); //запишет в атрибут class значение www
+// // 
+// // Должны работать цепочки методов:
+// // elem.html('hello').append('!').prepend('!');
+// // elem.attr('class', 'www').attr('title', 'hello');
+
+// -----------------------------------------------------
+// 2)  Реализуйте на ООП игру "морской бой" против компьютера.
+
+class Field{
+    constructor (field) {
+        this.field = field;
+    }
+    render(){
 
     }
-    //запишет в текст элемента '!'
-    html(text) {
-        this.selector.innerText += text;
-    }
-    // запишет в начало элемента '!'
-    append(text) {
-        document.body.appendChild(this.selector)
-    }
-    prepend(text) {
-        // this.selector.insertBefore(this.selector, text)
-    }
 }
-var elem = new Element('div');
-elem.html('!')//запишет в текст элемента '!'
-elem.append('!'); //запишет в начало элемента '!'
-elem.prepend('!'); //запишет в конец элемента '!'
-// elem.attr('class', 'www'); //запишет в атрибут class значение www
-// 
-// Должны работать цепочки методов:
-// elem.html('hello').append('!').prepend('!');
-// elem.attr('class', 'www').attr('title', 'hello');
+class SeaBattle extends Field{
+    constructor (field, role) {
+        super();
+        super.render();
+    }
+    render(){}
+}
+
+const userField = [
+    ['.','.','.','.','.','.','.','.','.','.'],
+    ['.','.','.','+','+','+','.','.','+','+'],
+    ['.','.','.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','+','.','.','.','.'],
+    ['.','.','.','.','.','.','.','+','+','+'],
+    ['.','.','.','.','+','.','.','.','.','.'],
+    ['.','.','.','.','+','.','+','+','.','.'],
+    ['.','.','.','.','+','.','.','.','.','.'],
+    ['.','+','+','.','+','.','+','.','+','.'],
+    ['.','.','.','.','.','.','.','.','.','.']
+]
+const compField = [
+    ['.','.','+','.','+','.','+','.','.','.'],
+    ['.','.','+','.','+','.','+','.','+','.'],
+    ['.','.','+','.','+','.','+','.','.','.'],
+    ['+','.','.','.','+','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.','.','.'],
+    ['.','.','.','.','.','.','.','.','+','.'],
+    ['.','.','.','.','.','.','.','.','.','.'],
+    ['.','+','.','.','.','.','.','.','+','.'],
+    ['.','+','.','.','.','.','.','.','.','.'],
+    ['.','.','.','+','+','.','+','+','.','.']
+]
+
+var game = new SeaBattle(userField, compField)
