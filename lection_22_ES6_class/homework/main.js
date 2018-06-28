d687c07f31da5da71d31bbfc47f64ec81547f61f
lenka-zelenka <elenazologina@gmail.com>
2018-06-28 21:16:03 +0300

    add sheeps count

--- lection_22_ES6_class/homework/main.js
+++ lection_22_ES6_class/homework/main.js
@@ -33,30 +33,43 @@ class Field {
         this.field = field;
         this.role = role;
 
-        var count = 0;
-        var firedItems = [];
+        var count = 0,
+            userCount = 0,
+            compCount = 20;
+        var userHint = document.getElementById('user-hint'),
+            compHint = document.getElementById('comp-hint');
+
+        userHint.innerText = userCount;
+        compHint.innerText = compCount;
 
         this.fire = (target) => {
+            userCount = document.querySelectorAll('#field-comp .broken').length;
             if (target.classList.contains('sheep')) {
                 target.classList.add('broken');
+                userCount += 1;
+
             } else {
                 target.classList.add('missed');
                 count += 1;
             }
+            if(userCount == 20) {
+                alert('You WIN!!!!')
+            }
             this.backFire();
+            userHint.innerText = userCount;
+            compHint.innerText = compCount;
         }
 
         this.backFire = () => {
             // функция устанавливает значение на поле юзера
             var targets = document.querySelectorAll('#field-user div');
             var sheeps = document.querySelectorAll('#field-user .sheep');
-
             if (count == 1 && sheeps.length > 0) {
                 let firedItemIndex = Math.floor(Math.random() * targets.length);
                 this.fire(targets[firedItemIndex]);
+                compCount = sheeps.length - document.querySelectorAll('#field-user .broken').length;
                 count = 0;
             }
-
             if (sheeps.length === 0) {
                 alert('You LOST')
             }
